a7bd5361299cc9011026e5bea2c6ee1638cb35ab
Bad <BadWolf42@users.noreply.github.com>
2023-09-28 19:45:12 +0200

    Fix verbose error logging

--- resources/jeeBrother.py
+++ resources/jeeBrother.py
@@ -1,23 +1,19 @@
-import asyncio
+from asyncio import run
 from datetime import datetime
 from brother import Brother, SnmpError, UnsupportedModel
-import json
+from json import dumps, JSONEncoder
 import logging
 from os import getenv
-import requests
+from requests import post
 from sys import argv
 
-class DateTimeEncoder(json.JSONEncoder):
+class DateTimeEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime):
             return o.isoformat()
-        return json.JSONEncoder.default(self, o)
+        return JSONEncoder.default(self, o)
 
 async def main():
-    if len(argv) <= 2:
-        logging.error('usage: %s <host> <ink/laser>', argv[0])
-        exit(1)
-
     newlevel = {
         'debug': logging.DEBUG,
         'info': logging.INFO,
@@ -26,19 +22,38 @@ async def main():
     }.get(getenv("LOGLEVEL", "error"), logging.ERROR)
     logging.basicConfig(level = newlevel)
 
+    logging.debug('VARS:')
+    logging.debug('  host:         %s', argv[1])
+    logging.debug('  printer_type: %s', argv[2])
+    logging.debug('  LOGLEVEL:     %s', getenv("LOGLEVEL", "error"))
+    logging.debug('  CALLBACK:     %s', getenv("CALLBACK", None))
+
+    if len(argv) <= 2:
+        logging.error('usage: %s <host> <ink/laser>', argv[0])
+        exit(1)
+
     callback = getenv("CALLBACK", None) # with APIKEY included
     if callback is None:
         logging.error('Missing callback url (use ENV var CALLBACK="<url>")')
         exit(2)
 
     try:
-        brother = Brother(argv[1], kind = argv[2])
+        brother = Brother(argv[1], kind=argv[2])
         data = await brother.async_update()
-    except (ConnectionError, SnmpError, UnsupportedModel) as e:
+    except (ConnectionError, SnmpError) as e:
+        logging.debug(f'{e}')
+        data = {'unreachable': True}
+    except UnsupportedModel as e:
         logging.error(f'{e}')
         data = {'unreachable': True}
 
-    r = requests.post(callback, json.dumps(data, cls=DateTimeEncoder))
+    r = post(callback, dumps(data, cls=DateTimeEncoder))
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+    # Run main task
+    try:
+        run(main())
+    except KeyboardInterrupt:
+        logging.info('Exiting')
+    except Exception:
+        logging.exception('Exception in main:')
