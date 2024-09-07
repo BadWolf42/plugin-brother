ecabf98cb75f228cbce5ebc0663bc5019bf81ba4
Bad <BadWolf42@users.noreply.github.com>
2024-09-07 13:59:50 +0200

    fix: paths in install script

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ######################### INCLUSION LIB ##########################
-#wget https://raw.githubusercontent.com/NebzHB/dependance.lib/master/dependance.lib --no-cache -O $BASEDIR/dependance.lib &>/dev/null
+#wget https://raw.githubusercontent.com/NebzHB/dependance.lib/master/dependance.lib --no-cache -O ${BASE_DIR}/dependance.lib &>/dev/null
 PROGRESS_FILENAME=$1
-PLUGIN=$(basename "$(realpath $BASEDIR/..)")
+PLUGIN=$(basename "$(realpath ${BASE_DIR}/..)")
 LANG_DEP=en
 TIMED=1
-. ${BASEDIR}/dependance.lib
+. ${BASE_DIR}/dependance.lib
 ##################################################################
-#wget https://raw.githubusercontent.com/NebzHB/dependance.lib/master/pyenv.lib --no-cache -O ${BASEDIR}/pyenv.lib &>/dev/null
-. ${BASEDIR}/pyenv.lib
+#wget https://raw.githubusercontent.com/NebzHB/dependance.lib/master/pyenv.lib --no-cache -O ${BASE_DIR}/pyenv.lib &>/dev/null
+. ${BASE_DIR}/pyenv.lib
 TARGET_PYTHON_VERSION="3.11"
-#VENV_DIR=${BASEDIR}/venv
+#VENV_DIR=${BASE_DIR}/venv
 #APT_PACKAGES="python3-venv python3-pip ..."
 ##################################################################
 
